5592e99613991489963bdabf5c4692ef15e605aa
jordan piepkow <jordanpiepkow@jordans-MacBook-Pro.local>
2020-05-10 13:25:59 -0400

    added repl

--- lib/jax.ex
+++ lib/jax.ex
@@ -28,8 +28,17 @@ defmodule Jax do
     Jax.Lexer.new(input)
     |> Jax.Lexer.read_tokens([])
   end
-
+  def repl(arr) do
+    IO.puts(">>")
+    line = IO.read(:stdio, :line)
+    tokens = Jax.Lexer.new(line) |> Jax.Lexer.read_tokens([])
+    Jax.repl(arr ++ tokens)
+  end
   def repl do
-    
+    IO.puts(">>")
+    line = IO.read(:stdio, :line)
+    tokens = Jax.Lexer.new(line)
+      |> Jax.Lexer.read_tokens([])
+    Jax.repl(tokens)
   end
 end
